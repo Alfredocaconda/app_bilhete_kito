30ba9836052ac9e88df756746d0ee6cd026b1bf1
kitoNzau <nzaualberto783@gmail.com>
2024-06-18 15:38:30 +0100

    feito

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -102,7 +102,7 @@
                                 <li class="">
                                     <a href="{{route('horario.index')}}" class="">
                                         <i class="fa fa-clock"></i>
-                                        <span class="ml-3">Horarios de Vigens</span>
+                                        <span class="ml-3">Horarios de Viagens</span>
                                     </a>
                                         <ul id="campaigns" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                                         </ul>
@@ -110,7 +110,7 @@
                                 <li class="">
                                     <a href="{{route('viagen.index')}}" class="">
                                         <i class="fa fa-suitcase-rolling"></i>
-                                        <span class="ml-3">Vigens</span>
+                                        <span class="ml-3">Viagens</span>
                                     </a>
                                         <ul id="campaigns" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                                         </ul>
